47e91c7ee2e8e8d56c25f7858228d3dd3a298570
hideayard <hideayard@gmail.com>
2024-06-28 00:31:14 +0700

    update script

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -14,6 +14,41 @@ if($local)
 {
   $url = 'http://project.local/itrust/web/close-order';
 }
+$account = Yii::$app->user->identity->user_account;
+$dashboardJS = <<<DASHBOARD_JS
+
+$('#outlook-btn').click(function(){
+    console.log('outlook-btn triggered');
+    $.post('$url/outlook', {
+        id : $account,
+    }, function(response){
+            console.log(response);
+          if (response.success) {
+                alert('Data saved successfully.');
+            } else {
+                alert('Failed to save data.');
+            }
+    });
+});
+
+$('#close-order-btn').click(function(){
+    console.log('close-order-btn triggered');
+    $.post('$url/close', {
+        id : $account,
+    }, function(response){
+            console.log(response);
+          if (response.success) {
+                alert('Data saved successfully.');
+            } else {
+                alert('Failed to save data.');
+            }
+    });
+});
+
+DASHBOARD_JS;
+
+$this->registerJs($dashboardJS);
+
 ?>
 
 <div class="card card-primary">
@@ -23,72 +58,10 @@ if($local)
 
     <div class="card-body">
     <p>ITrust Trading Platform </p>
-    <p> <button class="btn btn-info" onclick="getOutlook()"><span class="fa fa-image"></span> GET OUTLOOK</button> </p>
-    <p> <button class="btn btn-danger" onclick="closeOrder()"><span class="fa fa-trash"></span> CLOSE ALL</button> </p>
+    <p> <button class="btn btn-info" id="outlook-btn"><span class="fa fa-image"></span> GET OUTLOOK</button> </p>
+    <p> <button class="btn btn-danger" id="close-order-btn"><span class="fa fa-trash"></span> CLOSE ALL</button> </p>
     <!-- <p> <button class="btn btn-primary" onclick="addItem()"><span class="fa fa-trash"></span> CLOSE ALL</button> </p> -->
       
     </div>
 
 </div>
-
-<script>
-
-  function getOutlook()
-  {
-    // Example JavaScript/jQuery code to send POST request
-    $.ajax({
-      url: <?=$url.'/outlook'?>, // Replace with your controller route
-      // url: 'http://project.local/itrust/web/close-order/close', // Replace with your controller route
-        type: 'POST',
-        // dataType: 'json',
-        data: {
-            id: '<?= Yii::$app->user->identity->user_account ?>',
-        },
-        success: function(response) {
-            // Handle the success response
-            console.log(response.message);
-            if (response.success) {
-                alert('Data saved successfully.');
-            } else {
-                alert('Failed to save data.');
-            }
-        },
-        error: function(xhr, status, error) {
-            // Handle errors
-            console.error('Error:', error);
-            alert('Error occurred while saving data.');
-        }
-    });
-  }
-
-  function closeOrder()
-  {
-    // Example JavaScript/jQuery code to send POST request
-    $.ajax({
-        // url: 'https://itrustcare.id/itrust/web/close-order/close', // Replace with your controller route
-        url: <?=$url.'/close-order'?>, // Replace with your controller route
-
-        type: 'POST',
-        // dataType: 'json',
-        data: {
-            id: '<?= Yii::$app->user->identity->user_account ?>',
-        },
-        success: function(response) {
-            // Handle the success response
-            console.log(response.message);
-            if (response.success) {
-                alert('Data saved successfully.');
-            } else {
-                alert('Failed to save data.');
-            }
-        },
-        error: function(xhr, status, error) {
-            // Handle errors
-            console.error('Error:', error);
-            alert('Error occurred while saving data.');
-        }
-    });
-  }
-  
-
-</script>
