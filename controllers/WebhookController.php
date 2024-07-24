039dc46bd117c9dcccc51e69fa48cd27a5206e6d
hideayard <hideayard@gmail.com>
2024-07-25 00:20:01 +0700

    tes

--- controllers/WebhookController.php
+++ controllers/WebhookController.php
@@ -262,11 +262,16 @@ class WebhookController extends Controller
                             'reply_markup' => $encodedKeyboard
                         ]);
                     } else {
-                        $this->handleCallbackQuery($update['callback_query']);
-                        // if (preg_match('/^\/check(.*)$/', $callback_reply_to_message)) {
-                        //     // $this->matchCommand('check', $params);
-                        //     $this->check();
-                        // }
+                        // $this->handleCallbackQuery($update['callback_query']);
+                        if (preg_match('/^\/check(.*)$/', $callback_reply_to_message)) {
+                            // $this->matchCommand('check', $params);
+                            // $this->check();
+                            return TelegramHelper::answerCallbackQuery([
+                                'callback_query_id' => $update['callback_query']['id'],
+                                'text' => 'Ga boleh nakal',
+                                'show_alert' => true
+                            ]);
+                        }
                     }
                 } else {
                     return TelegramHelper::answerCallbackQuery([
