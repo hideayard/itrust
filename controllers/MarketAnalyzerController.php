1e463cc80c2da6effbbc99c2d4bdbe6ab877436e
AL <hideayard@gmail.com>
2025-12-24 12:41:57 +0700

    init market analyzer

---
+++ controllers/MarketAnalyzerController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Banner;
+use app\models\BannerSearch;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
+
+/**
+ * MarketAnalyzerController implements the CRUD actions for Banner model.
+ */
+class MarketAnalyzerController extends Controller
+{
+
+    public $layout = 'market-analyzer';
+
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                    'generate' => ['post'],
+                    'submit-section' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+    /**
+     * Lists all Banner models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
